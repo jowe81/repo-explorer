d52aad06bff1055083d569f7d94ddd2a3827f302
Johannes Weber <johannes@drweber.de>
2022-07-02 19:26:48 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RepoTable from './components/RepoTable';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <RepoTable />
+    </div>
+  );
+}
