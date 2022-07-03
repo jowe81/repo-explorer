a8406416d2f4a2cb5cb92aa00a831407033a11d1
Johannes Weber <johannes@drweber.de>
2022-07-02 19:40:11 -0700

    pull repo data from API

--- src/components/RepoTable.tsx
+++ src/components/RepoTable.tsx
@@ -1,17 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
 export default function RepoTable() {
+  const [repoData, setRepoData] = useState([]);
+
   useEffect(() => {
     axios
       .get('http://localhost:4000/repos')
       .then((res) => {
         console.log(res.data);
+        setRepoData(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [repoData]);
 
-  return <div>The table</div>;
+  return (
+    <Table striped={true} bordered={true} hover={true}>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Language</th>
+          <th>Fork Count</th>
+        </tr>
+      </thead>
+      <tbody />
+    </Table>
+  );
 }
