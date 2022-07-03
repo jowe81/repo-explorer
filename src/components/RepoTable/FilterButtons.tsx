ea31769ff6cbed4e0980864599f295a519809357
Johannes Weber <johannes@drweber.de>
2022-07-02 20:34:25 -0700

    add FilterButtons.tsx - display group of buttons

---
+++ src/components/RepoTable/FilterButtons.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+export default function FilterButtons(props: object[]) {
+  return (
+    <ButtonGroup>
+      <Button>First</Button>
+      <Button>Second</Button>
+      <Button>Third</Button>
+    </ButtonGroup>
+  );
+}
