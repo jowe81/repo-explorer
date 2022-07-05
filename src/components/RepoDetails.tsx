93ae1852e1af1676f809a339442e424526afc0f0
Johannes Weber <johannes@drweber.de>
2022-07-04 17:55:49 -0700

    test useApplicationData hook

--- src/components/RepoDetails.tsx
+++ src/components/RepoDetails.tsx
@@ -1,6 +1,9 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useOutletContext } from 'react-router-dom';
 
-export default function RepoDetails() {
+export default function RepoDetails(props: any) {
+  const appData: any = useOutletContext();
   const params = useParams();
+  console.log('loaded', appData.loaded);
+  console.log('Repo-Data', appData.repoData);
   return <div>Details for {params.repoId}</div>;
 }
