0aeb3a81ae475eb5bd4bffa2b5a77276787a5142
Johannes Weber <johannes@drweber.de>
2022-07-05 18:19:55 -0700

    set document.title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import useApplicationData from './hooks/useApplicationData';
 import './App.css';
 
 export function App() {
   const appData = useApplicationData();
+
+  useEffect(() => {
+    document.title = 'Repo-Explorer';
+  }, []);
 
   return (
     <div className="App">
