441d7eef4f5251fafa3db22733f69c81e5863a63
Johannes Weber <johannes@drweber.de>
2022-07-05 17:35:05 -0700

    refactor display of readme into Readme component

---
+++ src/components/RepoDetails/Readme.tsx
@@ -0,0 +1,24 @@
+import { useParams, useOutletContext } from 'react-router-dom';
+import useQuery from '../../hooks/useQuery';
+
+export default function RepoDetails(props: any) {
+  const appData: any = useOutletContext();
+  const params = useParams();
+
+  const repoData = appData.repoData.find((record: any) => {
+    return record.id === Number(params.repoId);
+  });
+
+  const readmeUrl = `https://raw.githubusercontent.com/${repoData.owner.login}/${repoData.name}/master/README.md`;
+  const [data, error, loading] = useQuery(readmeUrl);
+
+  return (
+    <div>
+      <div>
+        {error && <div>Could not load details...</div>}
+        {loading && <div>Loading...</div>}
+        {data}
+      </div>
+    </div>
+  );
+}
