bf76c610a273a5f5e9a29ddc222cc244222143ce
Johannes Weber <johannes@drweber.de>
2022-07-05 17:35:21 -0700

    refactor commit details into LatestCommit component

---
+++ src/components/RepoDetails/LatestCommit.tsx
@@ -0,0 +1,49 @@
+import { useParams, useOutletContext } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import useQuery from '../../hooks/useQuery';
+
+export default function RepoDetails(props: any) {
+  const appData: any = useOutletContext();
+  const params = useParams();
+
+  const repoData = appData.repoData.find((record: any) => {
+    return record.id === Number(params.repoId);
+  });
+
+  const commitsUrl = `https://api.github.com/repos/${repoData.owner.login}/${repoData.name}/commits`;
+  const [data, error, loading] = useQuery(commitsUrl);
+
+  const latestCommit = data?.length ? data[0] : undefined;
+
+  return (
+    <div>
+      <div>
+        {error && <div>Could not load details...</div>}
+        {loading && <div>Loading...</div>}
+        {data && (
+          <Table className="text-start">
+            <thead>
+              <tr>
+                <th colSpan={2}>Details for repo '{repoData.name}'</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Date:</td>
+                <td>{latestCommit?.commit.author.date}</td>
+              </tr>
+              <tr>
+                <td>Author:</td>
+                <td>{latestCommit?.commit.author.name}</td>
+              </tr>
+              <tr>
+                <td>Message:</td>
+                <td>{latestCommit?.commit.message}</td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
+      </div>
+    </div>
+  );
+}
