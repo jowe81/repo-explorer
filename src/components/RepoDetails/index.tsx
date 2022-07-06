236faad3c3ac87290a36b26822f9826913650f68
Johannes Weber <johannes@drweber.de>
2022-07-05 17:35:40 -0700

    refactor content to sub-components

--- src/components/RepoDetails/index.tsx
+++ src/components/RepoDetails/index.tsx
@@ -1,6 +1,7 @@
 import { useParams, useOutletContext, Link } from 'react-router-dom';
-import { Table, Button } from 'react-bootstrap';
-import useQuery from '../../hooks/useQuery';
+import { Button } from 'react-bootstrap';
+import LatestCommit from './LatestCommit';
+import Readme from './Readme';
 
 export default function RepoDetails(props: any) {
   const appData: any = useOutletContext();
@@ -9,50 +10,19 @@ export default function RepoDetails(props: any) {
   const repoData = appData.repoData.find((record: any) => {
     return record.id === Number(params.repoId);
   });
+  console.log(repoData);
+  //const repoFullName = `${repoData.owner.login}/${repoData.name}`;
+  //const readmeUrl = `https://raw.githubusercontent.com/${repoFullName}/master/README.md`;
 
-  const repoFullName = `${repoData.owner.login}/${repoData.name}`;
-  const commitsUrl = `https://api.github.com/repos/${repoFullName}/commits`;
-  const readmeUrl = `https://raw.githubusercontent.com/${repoFullName}/master/README.md`;
-
-  const readmeData = useQuery(readmeUrl).data;
-  const commits = useQuery(commitsUrl).data;
-
-  let content = <div>loading...</div>;
-
-  if (commits?.length) {
-    const latestCommit = commits[0];
-    content = (
-      <Table className="text-start">
-        <thead>
-          <tr>
-            <th colSpan={2}>Details for repo '{repoData.name}'</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Date:</td>
-            <td>{latestCommit?.commit.author.date}</td>
-          </tr>
-          <tr>
-            <td>Author:</td>
-            <td>{latestCommit?.commit.author.name}</td>
-          </tr>
-          <tr>
-            <td>Message:</td>
-            <td>{latestCommit?.commit.message}</td>
-          </tr>
-        </tbody>
-      </Table>
-    );
-  }
+  //const readmeData = useQuery(readmeUrl).data;
 
   return (
     <div>
       <Link to="/">
         <Button>Go back</Button>
       </Link>
-      <div>{content}</div>
-      <div>{readmeData}</div>
+      <LatestCommit />
+      <Readme />
     </div>
   );
 }
